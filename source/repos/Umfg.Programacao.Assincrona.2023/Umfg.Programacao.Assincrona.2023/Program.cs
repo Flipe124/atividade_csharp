d9fd574c67a4ff3428f35785a23d7bd3af7bf7e8
Felipe <flipeoliveira007@gmail.com>
2023-04-24 22:12:41 -0300

    Aula 24/04/2023

---
+++ source/repos/Umfg.Programacao.Assincrona.2023/Umfg.Programacao.Assincrona.2023/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Umfg.Programacao.Assincrona._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AguardarEmSegundosAsync(5);
+            Console.ReadLine();
+
+            for(var i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static async void AguardarEmSegundosAsync(int segundos)
+        {
+            Console.WriteLine("Inicio aguaradar processamento");
+            await Task.Delay(TimeSpan.FromSeconds(segundos));
+            Console.WriteLine($"Fim do processamento, aguardou {segundos}");
+        }
+    }
+}
